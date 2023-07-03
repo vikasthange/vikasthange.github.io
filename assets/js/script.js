b0bf23487e70a7e0c7502691e8c43c62a086c6f9
Vikas Thange <vikasthange@gmail.com>
2023-07-02 21:19:29 -0400

    formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,31 +160,31 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
 var messageForm = document.getElementById("my-form");
             
-            async function handleSubmit(event) {
-              event.preventDefault();
-              var status = document.getElementById("my-form-status");
-              var data = new FormData(event.target);
-              fetch(event.target.action, {
-                method: messageForm.method,
-                body: data,
-                headers: {
-                    'Accept': 'application/json'
-                }
-              }).then(response => {          
-                if (response.ok) {
-                  status.innerHTML = "Message sent! Thanks for your message!";
-                  messageForm.reset()
-                } else {
-                  response.json().then(data => {
-                    if (Object.hasOwn(data, 'errors')) {
-                      status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
-                    } else {
-                      status.innerHTML = "Oops! There was a problem sending your message";
-                    }
-                  })
-                }
-              }).catch(error => {
-                status.innerHTML = "Oops! There was a problem sending your message";
-              });
-            }
-            messageForm.addEventListener("submit", handleSubmit)
+async function handleSubmit(event) {
+event.preventDefault();
+var status = document.getElementById("my-form-status");
+var data = new FormData(event.target);
+fetch(event.target.action, {
+  method: messageForm.method,
+  body: data,
+  headers: {
+      'Accept': 'application/json'
+  }
+}).then(response => {          
+  if (response.ok) {
+    status.innerHTML = "Message sent! Thanks for your message!";
+    messageForm.reset()
+  } else {
+    response.json().then(data => {
+      if (Object.hasOwn(data, 'errors')) {
+        status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+      } else {
+        status.innerHTML = "Oops! There was a problem sending your message";
+      }
+    })
+  }
+}).catch(error => {
+  status.innerHTML = "Oops! There was a problem sending your message";
+});
+}
+messageForm.addEventListener("submit", handleSubmit)
